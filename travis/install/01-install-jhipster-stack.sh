fdb09bed32398818527f9d5325103da4932e96fe
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-07-27 19:14:56 +0200

    Fix checkout to branch

--- travis/install/01-install-jhipster-stack.sh
+++ travis/install/01-install-jhipster-stack.sh
@@ -16,7 +16,7 @@ if [ "$JHIPSTER_BRANCH" == "latest" ]; then
     LATEST=$(git describe --abbrev=0)
     git checkout -b $LATEST $LATEST
 elif [ "$JHIPSTER_BRANCH" != "master" ]; then
-    git checkout -b $JHIPSTER_BRANCH $JHIPSTER_BRANCH
+    git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
 fi
 git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 yarn install
