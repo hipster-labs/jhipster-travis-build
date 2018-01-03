394b5a06f1dd78a1e19d94cc3affd39cb6ad047c
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-01-03 08:41:40 +0100

    Add jhipster-dependencies repo

--- travis/install-jhipster.sh
+++ travis/install-jhipster.sh
@@ -2,10 +2,34 @@
 set -ev
 
 #-------------------------------------------------------------------------------
-# Choose the repo
+# Choose the repos
 #-------------------------------------------------------------------------------
-export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
-export JHIPSTER_BRANCH=master
+JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
+JHIPSTER_BRANCH=master
+
+# if the JHIPSTER_DEPENDENCIES_BRANCH value is release,
+# it won't clone the project but use the release from Maven
+JHIPSTER_DEPENDENCIES_REPO=https://github.com/jhipster/jhipster-dependencies.git
+JHIPSTER_DEPENDENCIES_BRANCH=release
+
+#-------------------------------------------------------------------------------
+# Install jhipster-dependencies
+#-------------------------------------------------------------------------------
+cd $TRAVIS_BUILD_DIR
+if [[ "$JHIPSTER_DEPENDENCIES_BRANCH" != "release" ]]; then
+    git clone "$JHIPSTER_DEPENDENCIES_REPO" jhipster-dependencies
+    cd jhipster-dependencies
+    if [ "$JHIPSTER_DEPENDENCIES_BRANCH" == "latest" ]; then
+        LATEST=$(git describe --abbrev=0)
+        git checkout -b $LATEST $LATEST
+    elif [ "$JHIPSTER_DEPENDENCIES_BRANCH" != "master" ]; then
+        git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
+    fi
+    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
+    ./mvnw clean install -Dgpg.skip=true
+fi
+ls -al ~/.m2/repository/io/github/jhipster/jhipster-dependencies/
 
 #-------------------------------------------------------------------------------
 # Install JHipster
@@ -20,5 +44,6 @@ elif [ "$JHIPSTER_BRANCH" != "master" ]; then
     git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
 fi
 git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
 yarn install
 yarn global add file:"$TRAVIS_BUILD_DIR"/generator-jhipster
