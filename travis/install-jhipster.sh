f85bbd47d91ccf670ac4f4459525a5b7e04dd007
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-01-06 11:22:37 +0100

    Remove HTTP Session Clustering with Hazelcast

--- travis/install-jhipster.sh
+++ travis/install-jhipster.sh
@@ -2,18 +2,7 @@
 set -ev
 
 #-------------------------------------------------------------------------------
-# Choose the repos
-#-------------------------------------------------------------------------------
-JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
-JHIPSTER_BRANCH=master
-
-# if the JHIPSTER_DEPENDENCIES_BRANCH value is release,
-# it won't clone the project but use the release from Maven
-JHIPSTER_DEPENDENCIES_REPO=https://github.com/jhipster/jhipster-dependencies.git
-JHIPSTER_DEPENDENCIES_BRANCH=release
-
-#-------------------------------------------------------------------------------
-# Install jhipster-dependencies
+# Install JHipster Dependencies
 #-------------------------------------------------------------------------------
 cd $TRAVIS_BUILD_DIR
 if [[ "$JHIPSTER_DEPENDENCIES_BRANCH" != "release" ]]; then
@@ -23,7 +12,7 @@ if [[ "$JHIPSTER_DEPENDENCIES_BRANCH" != "release" ]]; then
         LATEST=$(git describe --abbrev=0)
         git checkout -b $LATEST $LATEST
     elif [ "$JHIPSTER_DEPENDENCIES_BRANCH" != "master" ]; then
-        git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
+        git checkout -b $JHIPSTER_DEPENDENCIES_BRANCH origin/$JHIPSTER_DEPENDENCIES_BRANCH
     fi
     git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
@@ -32,7 +21,26 @@ if [[ "$JHIPSTER_DEPENDENCIES_BRANCH" != "release" ]]; then
 fi
 
 #-------------------------------------------------------------------------------
-# Install JHipster
+# Install JHipster lib
+#-------------------------------------------------------------------------------
+cd $TRAVIS_BUILD_DIR
+if [[ "$JHIPSTER_LIB_BRANCH" != "release" ]]; then
+    git clone "$JHIPSTER_LIB_REPO" jhipster
+    cd jhipster
+    if [ "$JHIPSTER_LIB_BRANCH" == "latest" ]; then
+        LATEST=$(git describe --abbrev=0)
+        git checkout -b $LATEST $LATEST
+    elif [ "$JHIPSTER_LIB_BRANCH" != "master" ]; then
+        git checkout -b $JHIPSTER_LIB_BRANCH origin/$JHIPSTER_LIB_BRANCH
+    fi
+    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
+    ./mvnw clean install -Dgpg.skip=true
+    ls -al ~/.m2/repository/io/github/jhipster/jhipster/
+fi
+
+#-------------------------------------------------------------------------------
+# Install JHipster Generator
 #-------------------------------------------------------------------------------
 cd $TRAVIS_BUILD_DIR
 git clone $JHIPSTER_REPO generator-jhipster
