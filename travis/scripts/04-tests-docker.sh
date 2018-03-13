d16d20f27c859f44192b164b8e8e352b9c113748
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-13 08:29:49 +0100

    Update Docker build

---
+++ travis/scripts/04-tests-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+#-------------------------------------------------------------------------------
+# Test Docker image
+#-------------------------------------------------------------------------------
+docker run --rm -it jhipster/jhipster:master yarn --version
+docker run --rm -it jhipster/jhipster:master yo --version
+docker run --rm -it jhipster/jhipster:master bower --version
+docker run --rm -it jhipster/jhipster:master gulp --version
+docker run --rm -it jhipster/jhipster:master jhipster --help --no-insight
+docker run --rm -it jhipster/jhipster:master jhipster info --no-insight
+
+#-------------------------------------------------------------------------------
+# Generate the project with Docker image
+#-------------------------------------------------------------------------------
+mkdir -p "$APP_FOLDER"
+cp -f "$JHIPSTER_SAMPLES"/"$JHIPSTER"/.yo-rc.json "$APP_FOLDER"/
+cd "$APP_FOLDER"
+
+docker run --rm -it -v "$APP_FOLDER":/home/jhipster/app \
+    jhipster/jhipster:master jhipster --force --no-insight --skip-checks --with-entities
+ls -al "$APP_FOLDER"
+
+docker run --rm -it -v "$APP_FOLDER":/home/jhipster/app ./mvnw test
+docker run --rm -it -v "$APP_FOLDER":/home/jhipster/app yarn test
