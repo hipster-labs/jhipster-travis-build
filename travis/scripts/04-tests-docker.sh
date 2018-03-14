a4c946dd8d44f340d701aea654d207a2cbb4a1b5
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-14 08:00:18 +0100

    Start container and run commands

--- travis/scripts/04-tests-docker.sh
+++ travis/scripts/04-tests-docker.sh
@@ -4,27 +4,16 @@ set -e
 #-------------------------------------------------------------------------------
 # Test Docker image
 #-------------------------------------------------------------------------------
-docker run --rm -it jhipster/jhipster:master yarn --version
-docker run --rm -it jhipster/jhipster:master yo --version
-docker run --rm -it jhipster/jhipster:master bower --version
-docker run --rm -it jhipster/jhipster:master gulp --version
-docker run --rm -it jhipster/jhipster:master jhipster --help --no-insight
-docker run --rm -it jhipster/jhipster:master jhipster info --no-insight
-
-#-------------------------------------------------------------------------------
-# Generate the project with Docker image
-#-------------------------------------------------------------------------------
-mkdir -p "$APP_FOLDER"
-cp -f "$JHIPSTER_SAMPLES"/"$JHIPSTER"/.yo-rc.json "$APP_FOLDER"/
-cd "$APP_FOLDER"
-ls -al $HOME
-ls -al $APP_FOLDER
-
-docker run --rm -it --privileged -v "$APP_FOLDER":/home/jhipster/app/ jhipster/jhipster:master ls -al
-
-docker run --rm -it --privileged -v "$APP_FOLDER":/home/jhipster/app/ \
-    jhipster/jhipster:master jhipster --force --no-insight --skip-checks --with-entities
-ls -al "$APP_FOLDER"
-
-docker run --rm -it -v "$APP_FOLDER":/home/jhipster/app ./mvnw test
-docker run --rm -it -v "$APP_FOLDER":/home/jhipster/app yarn test
+docker container run -d --name jhipster jhipster/jhipster:master
+docker container ps
+docker container exec jhipster yarn --version
+docker container exec jhipster yo --version
+docker container exec jhipster bower --version
+docker container exec jhipster gulp --version
+docker container exec jhipster jhipster --help --no-insight
+docker container exec jhipster jhipster info --no-insight
+docker container cp "$JHIPSTER_SAMPLES"/"$JHIPSTER"/.yo-rc.json jhipster:/home/jhipster/app/
+docker container exec jhipster ls -al
+docker container exec jhipster jhipster --force --no-insight --skip-checks --with-entities
+docker container exec jhipster ./mvnw test
+docker container exec jhipster yarn test
