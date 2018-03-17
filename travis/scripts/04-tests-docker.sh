13c263f1b1c3af7cbfd9c90e0a9933cfeb34e443
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-17 10:13:52 +0100

    Try to fix docker build

--- travis/scripts/04-tests-docker.sh
+++ travis/scripts/04-tests-docker.sh
@@ -12,7 +12,7 @@ docker container exec -it jhipster bower --version
 docker container exec -it jhipster gulp --version
 docker container exec -it jhipster jhipster --help --no-insight
 docker container exec -it jhipster jhipster info --no-insight
-docker container exec -it jhipster curl https://raw.githubusercontent.com/jhipster/generator-jhipster/master/travis/samples/ngx-default/.yo-rc.json -o toto.txt
+docker container exec -it jhipster curl https://raw.githubusercontent.com/jhipster/generator-jhipster/master/travis/samples/ngx-default/.yo-rc.json -o .yo-rc.json
 docker container exec -it jhipster ls -al
 docker container exec -it jhipster jhipster --force --no-insight --skip-checks --with-entities
 docker container exec -it jhipster ./mvnw test
